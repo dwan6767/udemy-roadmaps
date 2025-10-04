28ce18c34de08f6dade8754aacccff238dd6a1f6
dwan6767 <dwanshikari@.com>
2025-10-04 13:21:06 +0530

    Initial static site

---
+++ data/courses.js
@@ -0,0 +1,16 @@
+const courses = [
+  {
+    title: "Complete HTML & CSS Bootcamp",
+    image: "images/html.jpg",
+    description: "Learn HTML & CSS from scratch",
+    link: "https://affiliate.udemy.com/link/html-course",
+    stage: "Stage 1"
+  },
+  {
+    title: "JavaScript Basics",
+    image: "images/js.jpg",
+    description: "Learn JS fundamentals",
+    link: "https://affiliate.udemy.com/link/js-course",
+    stage: "Stage 2"
+  }
+];
---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Placeholder for future dynamic features
+console.log("JS loaded - add search/filter functionality here");
+
+// Example: You can later load courses from data/courses.js and render dynamically
